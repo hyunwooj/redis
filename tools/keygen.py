05c669457be5454c829cc37cb2e3ec51c1507bdc
Hyunwoo Jung <hyunwoojhw@gmail.com>
2019-11-19 01:22:26 +0900

    add key pattern: seq

--- tools/keygen.py
+++ tools/keygen.py
@@ -1,15 +1,20 @@
+import argparse
 import sys
 import numpy as np
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("Please provide the number of keys")
-        print("ex) %s (#_of_key)" % sys.argv[0])
-        sys.exit(1)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--pattern', type=str, choices=['seq', 'perm'])
+    parser.add_argument('-n', '--num-keys', type=int)
+    return parser.parse_args()
 
-    count = int(sys.argv[1])
-    keys = np.random.permutation(count)
+def main():
+    args = parse_args()
+    if (args.pattern == 'seq'):
+        keys = range(args.num_keys)
+    elif (args.pattern == 'perm'):
+        keys = np.random.permutation(args.num_keys)
     print(' '.join(map(lambda x: str(x), keys)))
 
 if __name__ == '__main__':
